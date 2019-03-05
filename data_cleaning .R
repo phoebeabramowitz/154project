2b975e1a0968cf9bf70b100790216b172615e5f0
Phoebe <phoebe.abramowitz@berkeley.edu>
2019-03-04 17:24:05 -0800

    More exploration
    made rmd seperate from code to look at graphs and such for data_cleaning, looked at some stuff

--- data_cleaning .R
+++ data_cleaning .R
@@ -1,8 +1,6 @@
-setwd("~/Desktop/Berkeley/Spring19/stat_154/154project")
+#setwd("~/Desktop/Berkeley/Spring19/stat_154/154project")
 
 mote_location <- read.delim("./data/mote-location-data.txt", sep="")
-
-datetime <- read.csv("./data/sonoma-dates", header=FALSE)
 
 #data retrieved over the wireless network
 data_net <- read.csv("./data/sonoma-data-net.csv")
@@ -10,5 +8,12 @@ data_net <- read.csv("./data/sonoma-data-net.csv")
 #data retrieved from the flash logs
 data_log <- read.csv("./data/sonoma-data-log.csv")
 
-#concatenated
+#simply concatenated
 data_all <- read.csv("./data/sonoma-data-all.csv")
+
+
+
+
+
+
+
