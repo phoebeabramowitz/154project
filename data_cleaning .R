8de919058b25ef3ea892e74ab1fe38e7d950aab1
Phoebe <phoebe.abramowitz@berkeley.edu>
2019-03-05 13:49:50 -0800

    more joins

--- data_cleaning .R
+++ data_cleaning .R
@@ -1,15 +1,15 @@
 #setwd("~/Desktop/Berkeley/Spring19/stat_154/154project")
 
 mote_location <- read.delim("./data/mote-location-data.txt", sep="")
-
 #data retrieved over the wireless network
 data_net <- read.csv("./data/sonoma-data-net.csv")
-
 #data retrieved from the flash logs
 data_log <- read.csv("./data/sonoma-data-log.csv")
-
 #simply concatenated
 data_all <- read.csv("./data/sonoma-data-all.csv")
+
+#nodeid and epoch together provides a unique identifier for one measure. 
+
 
 
 
