b51973ae17eb9f5216c0b5fe3a482f85029dbf9d
Omri Newman <omrinewman@gmail.com>
2019-03-07 01:33:51 -0800

    started the write up, and pasted some code into the .R

--- data_cleaning .R
+++ data_cleaning .R
@@ -1,30 +1,47 @@
 #setwd("~/Desktop/Berkeley/Spring19/stat_154/154project")
 
-###Takes in raw data files, outputs main dataframe data_main 
-###
-###
+#location data
 mote_location <- read.delim("./data/mote-location-data.txt", sep="")
-#<<<<<<< HEAD
 
+#datetime
 datetime <- read.csv("./data/sonoma-dates", header=FALSE)
 
-#data retrieved over the wireless network
-data_net<- read.csv("./data/sonoma-data-net.csv")
-
 #data retrieved from the flash logs
 data_log <- read.csv("./data/sonoma-data-log.csv")
 
-#concatenated
-#=======
 #data retrieved over the wireless network
 data_net <- read.csv("./data/sonoma-data-net.csv")
-#data retrieved from the flash logs
-data_log <- read.csv("./data/sonoma-data-log.csv")
+
 #simply concatenated
-#>>>>>>> 03edcb8bf1f34c0f0802daad34d3fc3e00bb03de
 data_all <- read.csv("./data/sonoma-data-all.csv")
 
-#nodeid and epoch together provides a unique identifier for one measure. 
+# Incident PAR histograms
+data_log <- filter(data_log, hamatop < 10*quantile(data_log$hamatop, na.rm=TRUE)[4])
+ggplot(data_net) +
+  geom_histogram(aes(x=hamatop),color="darkblue", fill="lightblue",binwidth=20)+
+  ggtitle("Net Incident PAR")+
+  theme_minimal()
+ggplot(data_log) +
+  geom_histogram(aes(x=hamatop),color="darkblue", fill="lightblue",binwidth=20)+
+  ggtitle("Log Incident PAR")+
+  theme_minimal()
 
+# Reflected PAR histograms
+ggplot(data_net) +
+  geom_histogram(aes(x=hamabot),color="darkblue", fill="lightblue",binwidth=6)+
+  ggtitle("Net Reflected PAR")+
+  theme_minimal()
+ggplot(data_log) +
+  geom_histogram(aes(x=hamabot),color="darkblue", fill="lightblue",binwidth=6)+
+  ggtitle("Log Reflected PAR")+
+  theme_minimal()
 
-
+# Voltage histograms
+ggplot(data_net) +
+  geom_histogram(aes(x=voltage),color="darkblue", fill="lightblue",binwidth=10)+
+  ggtitle("Net Unconverted Voltage")+
+  theme_minimal()
+ggplot(data_log) +
+  geom_histogram(aes(x=voltage),color="darkblue", fill="lightblue",binwidth=0.2)+
+  ggtitle("Log Voltage")+
+  theme_minimal()
