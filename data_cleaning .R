adab977504ecbf90a08d2d67e2009357718af0ed
Omri Newman <omrinewman@gmail.com>
2019-03-07 15:35:06 -0800

    Problem 2
    Almost done with problem 2

--- data_cleaning .R
+++ data_cleaning .R
@@ -60,8 +60,21 @@ data_log$hamabot <- data_log$hamabot*0.0185
 # Voltage
 data_net$voltage = data_net$voltage*12.33 / 1023
 
-# Removing missing data...?
+# Removing missing data...
 
+#Number of missing values for each variable(don't display this)
+sum(is.na(df$temp))
+sum(is.na(df$humid))
+sum(is.na(df$incident_PAR))
+sum(is.na(df$reflect_PAR))
+#Initial Number of rows missing any measurements
+na_vals <- df %>% filter(is.na(temp)) %>% filter(is.na(humid)) %>%
+  filter(is.na(incident_PAR)) %>% filter(is.na(reflect_PAR))
+#we dont want it to evaluate this value
+nrow(na_vals)
+#Remove missing Data rows
+df <- df %>% filter(!is.na(temp)) %>% filter(!is.na(humid)) %>%
+  filter(!is.na(incident_PAR)) %>% filter(!is.na(reflect_PAR))
 
 # Concatenate everything in data_net with everything in data_log
 # that isn't in data_net, defined by epoch and nodeid
@@ -87,3 +100,27 @@ main <- left_join(df, mote_location) %>%
   rename(direc = Direc) %>%
   rename(dist = Dist) %>%
   rename(tree = Tree)
+
+# using histogram and quantiles to visually identify easy outliers
+ggplot(main) +
+geom_histogram(aes(x=voltage),color="darkblue", fill="lightblue",binwidth=1)+
+  ggtitle("Voltage")+
+  theme_minimal()
+
+ggplot(main) +
+  geom_histogram(aes(x=temp),color="darkblue", fill="lightblue",binwidth=4)+
+  ggtitle("Temperature")+
+  theme_minimal()
+
+ggplot(main) +
+  geom_histogram(aes(x=humid),color="darkblue", fill="lightblue",binwidth=4)+
+  ggtitle("Humidity")+
+  theme_minimal()
+
+summary(main$reflect_PAR)
+summary(main$incident_PAR)
+
+# Remove outliers based on faulty voltage readings
+# removes 33,833 rows..
+data_main <- main %>%
+  filter(voltage >= 2.4 & voltage <= 3)
